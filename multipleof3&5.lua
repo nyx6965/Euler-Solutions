2dbd3cd9a763bc2d0d7c7ad79dd0df67e388cf2c
amr8644 <amr8644@gmail.com>
2023-01-14 23:05:15 +0300

    First Commit

---
+++ multipleof3&5.lua
@@ -0,0 +1,23 @@
+-- Problem #1
+
+-- A palindromic number reads the same both ways. The largest palindrome made 
+-- from the product of two 2-digit numbers is 9009 = 91 × 99.
+-- Find the largest palindrome made from the product of two 3-digit numbers.
+
+local product
+local reversed_num
+local largest_num = 0
+
+for i = 1, 1000, 1 do
+    for j = 1, 1000, 1 do
+        product = i * j
+        reversed_num = string.reverse( tostring(product) )
+        if tonumber( reversed_num ) == product then
+            if largest_num == 0 or  product > largest_num then
+                largest_num = product
+            end
+        end
+    end
+end
+
+print(largest_num)
