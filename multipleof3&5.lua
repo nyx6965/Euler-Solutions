ec31a71bd03b04196a7153c546ac2e4ad431438b
amr8644 <amr8644@gmail.com>
2023-01-15 15:37:46 +0300

    Fixed some bugs...

--- multipleof3&5.lua
+++ multipleof3&5.lua
@@ -4,20 +4,12 @@
 -- from the product of two 2-digit numbers is 9009 = 91 × 99.
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 
-local product
-local reversed_num
-local largest_num = 0
+local sum = 0
 
-for i = 1, 1000, 1 do
-    for j = 1, 1000, 1 do
-        product = i * j
-        reversed_num = string.reverse( tostring(product) )
-        if tonumber( reversed_num ) == product then
-            if largest_num == 0 or  product > largest_num then
-                largest_num = product
-            end
-        end
+for i = 1, 999, 1 do
+    if i % 3 == 0 or i % 5 == 0 then
+        sum = sum + i
     end
 end
 
-print(largest_num)
+print(sum)
--- palindorme.lua
+++ palindorme.lua
@@ -3,12 +3,20 @@
 -- we get 3, 5, 6 and 9. The sum of these multiples is 23.
 -- Find the sum of all the multiples of 3 or 5 below 1000 .
 
-local sum = 0
+local product
+local reversed_num
+local largest_num = 0
 
-for i = 1, 999, 1 do
-    if i % 3 == 0 or i % 5 == 0 then
-        sum = sum + i
+for i = 1, 1000, 1 do
+    for j = 1, 1000, 1 do
+        product = i * j
+        reversed_num = string.reverse( tostring(product) )
+        if tonumber( reversed_num ) == product then
+            if largest_num == 0 or  product > largest_num then
+                largest_num = product
+            end
+        end
     end
 end
 
-print(sum)
+print(largest_num)
