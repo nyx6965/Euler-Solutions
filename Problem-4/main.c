078f3a4eb62334253e516ae3697eb721f4bc10a1
amr8644 <amr8644@gmail.com>
2023-01-15 18:54:28 +0300

    Fixed some bugs in str_rev function

--- Problem-4/main.c
+++ Problem-4/main.c
@@ -8,12 +8,13 @@
 // from the product of two 2-digit numbers is 9009 = 91 × 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-char *str_rev(char *str);
+int str_rev(char *str);
 
 int main(void)
 {
-    char *product = malloc(1000);
-    // product[0]= "";
+    char str_product[1000];
+    int product;
+
     char *reversed_str;
     int result;
 
@@ -24,13 +25,15 @@ int main(void)
     {
         for (int j = 1; j < 100; j++)
         {
-            sprintf(product,"%d",i * j);           
-            reversed_str = str_rev(product);
-            // it
-            if (strcmp(&reversed_str, product) == 0) 
-            {
-                printf("%s-----Palindorme\n",product);
-            }
+            // int n;
+            // product = i * j;
+            sprintf(str_product,"%d",i * j);         
+            reversed_str = str_rev(str_product);
+            // printf("%d--------%d\n",product, reversed_str);
+            // if (product === atoi(reversed_str)) 
+            // {
+            //     printf("%s-----Palindorme\n",product);
+            // }
                 
             // else
             //    if (largest_num == 0 || atoi(product) > largest_num )
@@ -44,10 +47,11 @@ int main(void)
     return 0;
 }
 
-char *str_rev(char *str)
+int str_rev(char *str)
 {
     char a,b,tmp;
     char *reversed_str = malloc(strlen(str));
+    int value;
 
     for (int i = 1; i <= strlen(str); i++)
     {
@@ -59,7 +63,10 @@ char *str_rev(char *str)
 
         reversed_str[0] = "";
         reversed_str[i] = a;
+        reversed_str[strlen(str) + 1] = '\0';
     }
-  
-    return reversed_str;    
+    value = atoi(reversed_str);
+    printf("%i\n",value);    
+
+    return atoi(reversed_str);    
 }
