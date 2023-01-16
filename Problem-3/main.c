e34d120e47bd8ad8fd2593b2c6b5a926117301e4
amr8644 <amr8644@gmail.com>
2023-01-16 18:25:17 +0300

    Finished Problem 2 and 3

--- Problem-3/main.c
+++ Problem-3/main.c
@@ -4,7 +4,52 @@
 // What is the largest prime factor of the number 600851475143 ?
 
 
+int prime_numbers(int value);
+
 int main( void )
 {
+    long int num = 13195;
+    int flag, x,largest_num = 0;
     
+    for (int i = 1; i <= num; i++)
+    {
+        // Find the factors of num
+        if (num % i == 0)
+            // Find the prime number
+            x = prime_numbers(i);
+            // Find the largest prime number
+            if (largest_num == 0 || largest_num < x)
+                largest_num = x;
+    }
+
+    printf("Largest prime factor: %d\n",largest_num);
+    return largest_num;
+}
+
+int prime_numbers(int value)
+{
+
+  int n, i, flag = 0;
+  n = value;
+  // 0 and 1 are not prime numbers
+  // change flag to 1 for non-prime number
+  if (n == 0 || n == 1)
+    flag = 1;
+
+  for (i = 2; i <= n / 2; ++i)
+  {
+    // if n is divisible by i, then n is not prime
+    // change flag to 1 for non-prime number
+    if (n % i == 0)
+    {
+      flag = 1;
+      break;
+    }
+  }
+
+  // flag is 0 for prime numbers
+  if (flag == 0)
+    return n;
+
+  return 0;
 }
--- Problem-3/main.lua
+++ Problem-3/main.lua
@@ -1,3 +1,42 @@
 -- Problem #3
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600851475143 ?
+
+local num = 600851475143;
+local largest_num = 0
+local x = 0
+
+function PrimeNumbers(value)
+
+    local n = value
+    local flag = 0
+
+    if n == 0 or n == 1 then
+        flag = 1;
+    end
+
+    for i = 2, n / 2, 1 do
+        if n % i == 0 then
+            flag = 1
+            break
+        end
+    end
+
+    if flag == 0 then
+        return n
+    else
+        return 0
+    end
+end
+
+for i = 1, num, 1 do
+    if num % i == 0 then
+      print(i)
+      x = PrimeNumbers(i)
+      if largest_num == 0 or largest_num < x then
+        largest_num = x
+      end
+    end
+end
+
+print(largest_num)
