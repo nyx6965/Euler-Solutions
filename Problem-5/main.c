965938caba84904dbfe4cbb88519f45f997e62a1
amr8644 <amr8644@gmail.com>
2023-01-17 21:17:22 +0300

    Finished Problem 5

--- Problem-5/main.c
+++ Problem-5/main.c
@@ -7,51 +7,38 @@
 // What is the smallest positive number that is evenly 
 // divisible by all of the numbers from 1 to 20
 
-int GCD(int a);
-int LCM(int a ,int b);
-
+int GCD(int a,int b);
 int main()
 {
-    int num = 2521;
-    int product = 1;
-    int lc;
-    int smallest_num,flag = 0;
+    int result = 1;
     
-    for (int i = 1; i <= 10; i++)
+    for (int i = 1; i <= 20; i++)
     {
-        // printf("%d\n",x);
-        // GCD(i);
-        lc = i * i / GCD(i);
-        printf("%d",lc);
-        
-        // product =  GCD(i) * i;
-        // // int lc = i * i / GCD(i);
-
+        int x = GCD(i,result);
+        result = result / x * i;            
     }
-
+  
     return 0;
     
 }
 
-int GCD(int a)
+int GCD(int a, int b)
 {
     int value,largest_num = 0;
 
-    // if (a > b || a == b)
-    //     value = a;
-    // if (b > a)
-    //     value = b;
+    if (a > b || a == b)
+        value = a;
+    if (b > a)
+        value = b;
 
-    for (int i = 1; i <= a; i++)
+    for (int i = 1; i <= value; i++)
     {
-        if (a % i == 0)
+        if (a % i == 0 && b % i == 0 )
         {
             if (largest_num == 0 || largest_num < i)
                 largest_num = i;
         }
     }
-
-    // printf("%d\n",largest_num);
     return largest_num;
 }
 
--- Problem-5/main.lua
+++ Problem-5/main.lua
@@ -4,3 +4,33 @@
 
 --  What is the smallest positive number that is evenly 
 --  divisible by all of the numbers from 1 to 20?
+
+function GCD( a, b )
+    local value,largest_num = 0,0
+    if a > b then
+        value = a
+    end
+    if b > a then
+        value = b
+    end
+    for i = 1, value, 1 do
+        if a % i == 0 and b % i == 0 then
+            if largest_num == 0 or largest_num < i then
+                largest_num = i
+            end
+        end
+    end
+    return largest_num
+end
+
+
+local result = 1
+local x
+
+for i = 1, 10, 1 do
+    x = GCD(i ,result)
+    -- print(x)
+    result = x * i / result
+end
+
+print(result)
